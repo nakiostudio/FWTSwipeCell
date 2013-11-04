621b6588321bb842542f01af89754e631b12c6a4
Carlos Vidal Pallín <Nakio@MacBook-Air-de-Carlos.local>
2013-11-04 21:07:24 +0000

    created example tableViewController

---
+++ FWTSwipeCell-Example/Controllers/FWTExampleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWTExampleTableViewController.h
+//  FWTSwipeCell-Example
+//
+//  Created by Carlos Vidal Pallín on 04/11/2013.
+//  Copyright (c) 2013 Future Workshops Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTExampleTableViewController : UITableViewController
+
+@end
