ebae58a47ccd4cb25f1e6a47f256ff1c17564480
Carlos Vidal Pallín <Nakio@MacBook-Air-de-Carlos.local>
2013-11-04 21:08:23 +0000

    created FWTSwipeCell class

---
+++ FWTSwipeCell-Example/FWTSwipeCell.m
@@ -0,0 +1,29 @@
+//
+//  FWTSwipeCell.m
+//  FWTSwipeCell-Example
+//
+//  Created by Carlos Vidal Pallín on 04/11/2013.
+//  Copyright (c) 2013 Future Workshops Ltd. All rights reserved.
+//
+
+#import "FWTSwipeCell.h"
+
+@implementation FWTSwipeCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
