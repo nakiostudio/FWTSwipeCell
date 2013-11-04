e45cb6ef67c2f3c95145fe24fe0c7a09f5efb21e
Carlos Vidal Pallín <Nakio@MacBook-Air-de-Carlos.local>
2013-11-04 21:36:30 +0000

    completed exampleTableView datasource

--- FWTSwipeCell-Example/Controllers/FWTExampleTableViewController.m
+++ FWTSwipeCell-Example/Controllers/FWTExampleTableViewController.m
@@ -8,7 +8,32 @@
 
 #import "FWTExampleTableViewController.h"
 
+@interface FWTExampleObject : NSObject
+
+@property (nonatomic, strong) NSDate *entryDate;
+
+@end
+
+@implementation FWTExampleObject
+
+@synthesize entryDate;
+
+@end
+
+
+
+NSString * kNavigationBarTitleText = @"FWTSwipeCell";
+
+typedef enum{
+    FWTExampleTableViewUnarchivedSection = 0,
+    FWTExampleTableViewArchivedSection,
+    FWTExampleTableViewSectionCount
+} FWTExampleTableViewSection;
+
 @interface FWTExampleTableViewController ()
+
+@property (nonatomic, strong) NSMutableArray *unarchivedEntries;
+@property (nonatomic, strong) NSMutableArray *archivedEntries;
 
 @end
 
@@ -17,27 +42,107 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self _setupNavigationBar];
+    [self _setupTableView];
 }
 
-#pragma mark - Table view data source
+#pragma mark - Private methods
+- (void)_setupNavigationBar
+{
+    self.title = kNavigationBarTitleText;
+    
+    UIBarButtonItem *addEntryButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                        target:self
+                                                                                        action:@selector(_addEntry)];
+    self.navigationItem.rightBarButtonItem = addEntryButtonItem;
+}
 
+- (void)_setupTableView
+{
+    [self.tableView setTableFooterView:[[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 1)]];
+}
+
+- (void)_addEntry
+{
+    FWTExampleObject *exampleObject = [[FWTExampleObject alloc] init];
+    exampleObject.entryDate = [NSDate date];
+    
+    [self.unarchivedEntries addObject:exampleObject];
+    
+    [self.tableView reloadData];
+}
+
+- (id)_objectForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+    FWTExampleObject *cellObject;
+    
+    switch (indexPath.section) {
+        case FWTExampleTableViewUnarchivedSection:
+            cellObject = self.unarchivedEntries[indexPath.row];
+            break;
+        case FWTExampleTableViewArchivedSection:
+            cellObject = self.archivedEntries[indexPath.row];
+            break;
+        default:
+            break;
+    }
+    
+    return cellObject;
+}
+
+#pragma mark - UITableViewDataSource methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 0;
+    return FWTExampleTableViewSectionCount;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    switch (section) {
+        case FWTExampleTableViewUnarchivedSection:
+            return self.unarchivedEntries.count;
+        case FWTExampleTableViewArchivedSection:
+            return self.archivedEntries.count;
+        default:
+            return 0;
+    }
 }
 
+#pragma mark - UITableViewDelegate methods
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    FWTExampleObject *cellObject = [self _objectForRowAtIndexPath:indexPath];
     
+    NSString *cellIdentifier = NSStringFromClass([UITableViewCell class]);
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = cellObject.debugDescription;
     
     return cell;
+}
+
+#pragma mark - Lazy loading
+- (NSMutableArray*)unarchivedEntries
+{
+    if (self->_unarchivedEntries == nil){
+        self->_unarchivedEntries = [NSMutableArray array];
+    }
+    
+    return self->_unarchivedEntries;
+}
+
+- (NSMutableArray*)archivedEntries
+{
+    if (self->_archivedEntries == nil){
+        self->_archivedEntries = [NSMutableArray array];
+    }
+    
+    return self->_archivedEntries;
 }
 
 @end
