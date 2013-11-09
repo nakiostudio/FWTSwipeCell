66b8394be471bbf054ba7774cefe08835d83b1f3
Carlos Vidal Pallín <Nakio@MacBook-Air-de-Carlos.local>
2013-11-09 21:03:43 +0000

    created webviewcontroller class

---
+++ FWTSwipeCell-Example/Controllers/FWTWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWTWebViewController.h
+//  FWTSwipeCell-Example
+//
+//  Created by Carlos Vidal Pallín on 09/11/2013.
+//  Copyright (c) 2013 Future Workshops Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTWebViewController : UIViewController
+
+@end
---
+++ FWTSwipeCell-Example/Controllers/FWTWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  FWTWebViewController.m
+//  FWTSwipeCell-Example
+//
+//  Created by Carlos Vidal Pallín on 09/11/2013.
+//  Copyright (c) 2013 Future Workshops Ltd. All rights reserved.
+//
+
+#import "FWTWebViewController.h"
+
+@interface FWTWebViewController ()
+
+@end
+
+@implementation FWTWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
