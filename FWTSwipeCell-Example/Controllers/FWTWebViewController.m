1a52c1c0b19710fbf9507aa6bf0b8aa2ec3b712a
Carlos Vidal Pallín <Nakio@MacBook-Air-de-Carlos.local>
2013-11-09 21:16:43 +0000

    implemented webViewController

--- FWTSwipeCell-Example/Controllers/FWTWebViewController.m
+++ FWTSwipeCell-Example/Controllers/FWTWebViewController.m
@@ -8,31 +8,64 @@
 
 #import "FWTWebViewController.h"
 
-@interface FWTWebViewController ()
+NSString * const kViewControllerTitle   =   @"About";
+NSString * const kWebViewRequestURL     =   @"http://www.futureworkshops.com";
+
+@interface FWTWebViewController () <UIWebViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
 
 @end
 
 @implementation FWTWebViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (void)dealloc
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    [self->_webView stopLoading];
+    [self->_webView setDelegate:nil];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    self.webView.delegate = self;
+    [self _setupNavigationBar];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidAppear:animated];
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:kWebViewRequestURL]]];
+}
+
+#pragma mark - Private methods
+- (void)_setupNavigationBar
+{
+    self.title = kViewControllerTitle;
+    
+    UIBarButtonItem *closeBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(_dismissScreen)];
+    self.navigationItem.rightBarButtonItem = closeBarButtonItem;
+}
+
+- (void)_dismissScreen
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Lazy loading
+- (UIWebView*)webView
+{
+    if (self->_webView == nil){
+        self->_webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame))];
+    }
+    
+    if (self->_webView.superview == nil){
+        [self.view addSubview:self->_webView];
+    }
+    
+    return self->_webView;
 }
 
 @end
